e99104da85a4babc513f36f42d7e3aaef24c7b63
jacobhsu <jacob.hsu.tw@gmail.com>
2021-12-16 10:32:15 +0800

    refacror: @pancakeswap/uikit npm-react-uikit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Router } from 'react-router-dom'
+import { ResetCSS } from 'npm-react-uikit'  // '@pancakeswap/uikit'
 import history from './routerHistory'
 import './App.css';
 import Home from './views/Home';
@@ -8,6 +9,7 @@ import Home from './views/Home';
 const App: React.FC = () => {
   return (
     <Router history={history}>
+      <ResetCSS />
       <Home />
     </Router>
   );
