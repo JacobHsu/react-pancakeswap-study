a8968a443491a042259b92753aae658dac88b924
jacobhsu <jacob.hsu.tw@gmail.com>
2021-12-24 10:49:11 +0800

    feat: GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Router } from 'react-router-dom'
-import { ResetCSS } from 'npm-react-uikit'  // '@pancakeswap/uikit'
+import { ResetCSS } from 'npm-react-uikit' // '@pancakeswap/uikit'
 import history from './routerHistory'
+import GlobalStyle from './style/Global'
 import './App.css';
 import Home from './views/Home';
 
@@ -10,6 +11,7 @@ const App: React.FC = () => {
   return (
     <Router history={history}>
       <ResetCSS />
+      <GlobalStyle />
       <Home />
     </Router>
   );
