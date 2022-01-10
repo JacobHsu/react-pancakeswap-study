2aef31f8813275f44b41fa4f6ea891bacbfdda39
jacobhsu <jacob.hsu.tw@gmail.com>
2022-01-10 15:42:50 +0800

    feat: UikitMenu links

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,13 @@
 import { Menu as UikitMenu } from 'npm-react-uikit' // '@pancakeswap/uikit'
+import { useTranslation } from 'contexts/Localization'
+import config from './config/config'
 
 const Menu = (props) => {
+  const { t } = useTranslation()
   return (
     <UikitMenu
-      // {...props}
+      links={config(t)}
+      {...props}
     />
   )
 }
