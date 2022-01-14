8888f239454309831ec55205302c514fcb99a827
jacobhsu <jacob.hsu.tw@gmail.com>
2022-01-14 14:33:49 +0800

    feat: Menu LangSelector

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -36,61 +36,61 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
       },
     ],
   },
-  {
-    label: t('Win'),
-    href: '/prediction',
-    icon: 'Trophy',
-    items: [
-      {
-        label: t('Prediction (BETA)'),
-        href: '/prediction',
-      },
-      {
-        label: t('Lottery'),
-        href: '/lottery',
-      },
-    ],
-  },
-  {
-    label: '',
-    href: '/info',
-    icon: 'More',
-    hideSubNav: true,
-    items: [
-      {
-        label: t('Info'),
-        href: '/info',
-      },
-      {
-        label: t('IFO'),
-        href: '/ifo',
-      },
-      {
-        label: t('Voting'),
-        href: '/voting',
-      },
-      {
-        type: DropdownMenuItemType.DIVIDER,
-      },
-      {
-        label: t('Leaderboard'),
-        href: '/teams',
-      },
-      {
-        type: DropdownMenuItemType.DIVIDER,
-      },
-      {
-        label: t('Blog'),
-        href: 'https://medium.com/pancakeswap',
-        type: DropdownMenuItemType.EXTERNAL_LINK,
-      },
-      {
-        label: t('Docs'),
-        href: 'https://docs.pancakeswap.finance',
-        type: DropdownMenuItemType.EXTERNAL_LINK,
-      },
-    ],
-  },
+  // {
+  //   label: t('Win'),
+  //   href: '/prediction',
+  //   icon: 'Trophy',
+  //   items: [
+  //     {
+  //       label: t('Prediction (BETA)'),
+  //       href: '/prediction',
+  //     },
+  //     {
+  //       label: t('Lottery'),
+  //       href: '/lottery',
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: '',
+  //   href: '/info',
+  //   icon: 'More',
+  //   hideSubNav: true,
+  //   items: [
+  //     {
+  //       label: t('Info'),
+  //       href: '/info',
+  //     },
+  //     {
+  //       label: t('IFO'),
+  //       href: '/ifo',
+  //     },
+  //     {
+  //       label: t('Voting'),
+  //       href: '/voting',
+  //     },
+  //     {
+  //       type: DropdownMenuItemType.DIVIDER,
+  //     },
+  //     {
+  //       label: t('Leaderboard'),
+  //       href: '/teams',
+  //     },
+  //     {
+  //       type: DropdownMenuItemType.DIVIDER,
+  //     },
+  //     {
+  //       label: t('Blog'),
+  //       href: 'https://medium.com/pancakeswap',
+  //       type: DropdownMenuItemType.EXTERNAL_LINK,
+  //     },
+  //     {
+  //       label: t('Docs'),
+  //       href: 'https://docs.pancakeswap.finance',
+  //       type: DropdownMenuItemType.EXTERNAL_LINK,
+  //     },
+  //   ],
+  // },
 ]
 
 export default config
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,11 +1,15 @@
 import { Menu as UikitMenu } from 'npm-react-uikit' // '@pancakeswap/uikit'
+import { languageList } from 'config/localization/languages'
 import { useTranslation } from 'contexts/Localization'
 import config from './config/config'
 
 const Menu = (props) => {
-  const { t } = useTranslation()
+  const { currentLanguage, setLanguage, t } = useTranslation()
   return (
     <UikitMenu
+      currentLang={currentLanguage.code}
+      langs={languageList}
+      setLang={setLanguage}
       links={config(t)}
       {...props}
     />
