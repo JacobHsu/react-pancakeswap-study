5bd7663c85e962384ed3de57f47a538ec2652345
jacobhsu <jacob.hsu.tw@gmail.com>
2022-01-18 17:40:58 +0800

    feat: UikitMenu activeItem

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,29 @@
-import { Menu as UikitMenu } from 'npm-react-uikit' // '@pancakeswap/uikit'
+import { useLocation } from 'react-router'
+import { Menu as UikitMenu } from  'npm-react-uikit' // '@pancakeswap/uikit'
 import { languageList } from 'config/localization/languages'
 import { useTranslation } from 'contexts/Localization'
 import useTheme from 'hooks/useTheme'
 import config from './config/config'
+import GlobalSettings from './GlobalSettings'
+import { getActiveMenuItem } from './utils'
 
 const Menu = (props) => {
-  const { toggleTheme } = useTheme()
+  const { isDark, toggleTheme } = useTheme()
+
   const { currentLanguage, setLanguage, t } = useTranslation()
+  const { pathname } = useLocation()
+  const activeMenuItem = getActiveMenuItem({ menuConfig: config(t), pathname })
+
   return (
     <UikitMenu
+      globalMenu={<GlobalSettings />}
+      isDark={isDark}
       toggleTheme={toggleTheme}
       currentLang={currentLanguage.code}
       langs={languageList}
       setLang={setLanguage}
       links={config(t)}
+      activeItem={activeMenuItem?.href}
       {...props}
     />
   )
